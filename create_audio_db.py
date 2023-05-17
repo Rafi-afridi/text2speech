abcb31446058be7a9431266d937cc1a33fdbf555
unknown <rafiafridi783@gmail.com>
2023-05-18 01:10:05 +0500

    code added

---
+++ create_audio_db.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pyttsx3
+
+def get_engine(rate, volume, voice):
+
+    # initialize pyttsx3 engine
+    engine = pyttsx3.init()
+
+    engine.setProperty('rate', rate)
+    engine.setProperty('volume', volume)
+    voices = engine.getProperty('voices')
+    if voice == 'Male':
+        engine.setProperty('voice', voices[0].id)
+    else:
+        engine.setProperty('voice', voices[1].id)
+    return engine
+
+# define Streamlit app
+def app():
+    # create sidebar
+    st.sidebar.header("Voice Settings")
+    rate = st.sidebar.slider("Rate", min_value=40, max_value=500, value=100, step=10)
+    volume = st.sidebar.slider("Volume", min_value=0.0, max_value=1.0, value=0.5, step=0.1)
+    voice = st.sidebar.radio("Voice", options=['Male', 'Female'])
+
+    # create main app
+    st.title("Text to Speech App")
+    st.write("Enter text and press the button to hear it spoken.")
+
+    # create text input and button
+    text = st.text_input("Text to speak")
+    if st.button("Speak"):
+        # get pyttsx3 engine based on user settings
+        engine = get_engine(rate, volume, voice)
+        # speak text
+        engine.say(text)
+        engine.runAndWait()
+
+if __name__ == '__main__':
+    app()
